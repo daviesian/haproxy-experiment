3b816b290e79abe6e4c27398c7d456e5b544ef32
Ian Davies <git@iandavies.org>
2017-10-20 20:48:09 +0100

    Update config

--- app/server.py
+++ app/server.py
@@ -1,15 +1,44 @@
-from flask import Flask
+from flask import Flask, make_response, request, abort
 import os
+import uuid
 
 app = Flask(__name__)
 
+up = True
+sessions = set()
+
 @app.route("/")
 def hello():
-    return "Hello from %s" % os.environ.get("SERVER_ID")
+    requested_session = request.args.get('s', request.cookies.get('SESSIONID'))
+    sid = requested_session if requested_session in sessions else str(uuid.uuid4())
+    sessions.add(sid)
+    resp = make_response("Hello from %s, session %s" % (os.environ.get("SERVER_ID"), sid))
+    resp.set_cookie('SESSIONID', sid)
+    return resp
 
 @app.route("/_status")
 def status():
-    return "OK"
+    if up:
+        return "OK"
+    else:
+        return "Nope", 404 #abort(404)
+
+@app.route("/sleep")
+def sleep():
+    global up
+    up = False
+    return ""
+
+@app.route("/wake")
+def wake():
+    global up
+    up = True
+    return ""
+
+@app.route("/die")
+def die():
+    request.environ.get('werkzeug.server.shutdown')()
+    return ""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
