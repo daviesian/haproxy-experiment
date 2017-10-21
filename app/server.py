92ec9af19897a522587f1ac12edb27755681b487
Ian Davies <git@iandavies.org>
2017-10-21 13:20:07 +0100

    Now with scaling

--- app/server.py
+++ app/server.py
@@ -1,10 +1,12 @@
 from flask import Flask, make_response, request, abort
 import os
 import uuid
+import signal
+import time
 
 app = Flask(__name__)
 
-up = True
+up = False
 sessions = set()
 
 @app.route("/")
@@ -12,7 +14,11 @@ def hello():
     requested_session = request.args.get('s', request.cookies.get('SESSIONID'))
     sid = requested_session if requested_session in sessions else str(uuid.uuid4())
     sessions.add(sid)
-    resp = make_response("Hello from %s, session %s" % (os.environ.get("SERVER_ID"), sid))
+
+    server = request.headers["X-Backend-Server"]
+
+    resp = make_response("Hello from %s, session %s.\n\n%s" % (server, sid, request.headers))
+    resp.headers["Content-Type"] = "text/plain"
     resp.set_cookie('SESSIONID', sid)
     return resp
 
@@ -41,4 +47,6 @@ def die():
     return ""
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGUSR1, lambda *a: wake())
+    signal.signal(signal.SIGUSR2, lambda *a: sleep())
     app.run(host="0.0.0.0")
